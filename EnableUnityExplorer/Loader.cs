11dba1c7cef80bf167ebfae046f87e1ab4e1b18c
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-04-26 19:31:11 +0100

    Add EnableUnityExplorer
    Adds usage of the UnityExplorer by https://github.com/sinai-dev

---
+++ EnableUnityExplorer/Loader.cs
@@ -0,0 +1,19 @@
+﻿using Winch.Core.API;
+using UnityExplorer;
+using System;
+
+namespace EnableUnityExplorer
+{
+    public class Loader
+    {
+        public static void Initialize ()
+        {
+            DredgeEvent.ManagersLoaded += startUnityExplorer;
+        }
+
+        public static void startUnityExplorer(object sender, EventArgs e)
+        {
+            ExplorerStandalone.CreateInstance();
+        }
+    }
+}
